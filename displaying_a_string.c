2671abe285990474b31fccfd6b4f69d25650bcee
jmwwangi <mwangijay46@gmail.com>
2025-11-19 09:37:50 +0300

    Update displaying_a_string.c

--- displaying_a_string.c
+++ displaying_a_string.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+int main(){
+    char name[50];
+    int length;
+
+    printf("Enter your name: ");
+    scanf(" %s", &name);
+
+    length = strlen(name);
+    printf("Your name is %s and has %i letters", name,length);
+
+    return 0;
+}
